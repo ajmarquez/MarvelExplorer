96bc8661df3149cc9122141423846209bef8cb16
Abelardo Marquez <abelardo@hey.com>
2021-03-19 12:42:12 +0100

    Add Folder structure

---
+++ MarvelExplorer/MarvelExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelExplorerApp.swift
+//  MarvelExplorer
+//
+//  Created by Abelardo Marquez on 19.03.21.
+//
+
+import SwiftUI
+
+@main
+struct MarvelExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
